1c0534ba99117a60cac158c7a1e5c0e3fb37f22f
Jude He <>
2025-12-08 23:09:32 -0500

    add additional controls

--- src/pages/examples/wobble/_Demo.tsx
+++ src/pages/examples/wobble/_Demo.tsx
@@ -11,33 +11,47 @@ import { useControls } from "leva";
 
 export function R3F() {
   const distort = useControls("Distort", {
-    intensity: { value: 1, min: 0, max: 2 },
+    intensity: { value: 0.75, min: 0, max: 2 },
     speed: { value: 10, min: 0, max: 20 },
   });
-  const sphere = useControls("Sphere", {
+  const geometry = useControls("Sphere", {
     size: { value: 1, min: 0, max: 5 },
     segments: { value: 50, min: 0, max: 100 },
+    isSphere: true,
   });
 
   const light = useControls("Light", {
     intensity: { value: 2, min: 0, max: 10 },
   });
+
+  const colors = useControls("Colors", {
+    start: { value: "aquamarine" },
+    middle: { value: "purple" },
+    end: { value: "hotpink" },
+  });
+
   return (
     <div id="canvas-container">
       <Canvas className="example">
         <CubeCamera position={[0, 0, 100]} />
         <ambientLight intensity={light.intensity} />
         <mesh>
-          <sphereGeometry
-            args={[sphere.size, sphere.segments, sphere.segments]}
-          />
+          {geometry.isSphere ? (
+            <sphereGeometry
+              args={[geometry.size, geometry.segments, geometry.segments]}
+            />
+          ) : (
+            <circleGeometry
+              args={[geometry.size, geometry.segments, geometry.segments]}
+            />
+          )}
           <MeshDistortMaterial
             distort={distort.intensity}
             speed={distort.speed}
           >
             <GradientTexture
               stops={[0, 0.5, 1]}
-              colors={["aquamarine", "purple", "hotpink"]}
+              colors={[colors.start, colors.middle, colors.end]}
               size={1024}
             />
           </MeshDistortMaterial>
